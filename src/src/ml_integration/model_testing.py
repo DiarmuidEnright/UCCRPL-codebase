eb3a2cbdc316434017bbcb32c6ae688e329270fe
Diarmuid Enright <diarmuidenright29@gmail.com>
2024-09-02 19:15:08 +0100

    model_testing final pushed

--- src/src/ml_integration/model_testing.py
+++ src/src/ml_integration/model_testing.py
@@ -1,11 +1,9 @@
 from rocketpy import Environment, SolidMotor, Rocket, Flight
-from settings.py import thrust_source
+from google.colab import files
 import datetime
 
+#Environment setup
 
-'''
-Environment setup
-'''
 env = Environment(latitude=51.89291, longitude=-8.49289, elevation=13)
 tomorrow = datetime.date.today() + (datetime.timedelta(days = 1))
 env.set_date((tomorrow.year, tomorrow.month, tomorrow.day, 12))
@@ -30,3 +28,87 @@ PRO75L645 = SolidMotor(
     throat_radius=12.5 / 1000,
     coordinate_system_orientation="nozzle_to_combustion_chamber"
 )
+
+rocket = Rocket(
+    radius=106 / 2000,
+    mass=5.109,
+    inertia=(3.8 , 3.8, 0.01),
+
+    power_off_drag = "DragOffCSV.csv",
+
+    power_on_drag = "DragOnCSV.csv",
+
+    center_of_mass_without_motor=86.7 / 100,
+
+    coordinate_system_orientation="tail_to_nose"
+)
+
+rail_buttons = rocket.set_rail_buttons(
+    upper_button_position=0.618,
+    lower_button_position=0.230,
+    angular_position=45,
+)
+
+rocket.add_motor(PRO75L645, position=0)
+
+nose_cone = rocket.add_nose(length=0.484, kind="vonKarman", position=1.77)
+
+fin_set = rocket.add_trapezoidal_fins(
+    n=3, #fin num
+    root_chord=0.25,
+    tip_chord=0.098,
+    span=0.15,
+    position=0.32,
+    cant_angle=0,
+    airfoil=("AirfoilDegreesCSV.csv", "degrees"),
+)
+
+tail = rocket.add_tail(
+    top_radius = 106 / 2000,
+    bottom_radius=0.04,
+    length=0.07,
+    position=0.07
+)
+
+rocket.all_info()
+
+
+Main = rocket.add_parachute(
+    "Main",
+    cd_s = 1.0,
+    trigger = 200,
+    sampling_rate = 105,
+    lag = 1.5,
+    noise = (0, 8.3, 0.5),
+)
+
+Drogue = rocket.add_parachute(
+    "Drogue",
+    cd_s = 0.313,
+    trigger = "apogee",
+    sampling_rate = 105,
+    lag = 1.5,
+    noise = (0, 8.3, 0.5),
+)
+
+#simulating a flight
+
+test_flight = Flight(
+    rocket=rocket,
+    environment=env,
+    rail_length=4,
+    inclination=70,
+    heading = 170
+)
+
+test_flight.all_info()
+
+from google.colab import files
+
+test_flight.export_kml(
+    file_name="Trajectory.kml",
+    extrude=True,
+    altitude_mode="relative_to_ground",
+)
+
+files.download('Trajectory.kml')

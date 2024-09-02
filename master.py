235bf852b081c5ada88d3ceec36316dcd2c2ba95
Diarmuid Enright <diarmuidenright29@gmail.com>
2024-09-02 02:19:54 +0100

    Updated master.py

--- master.py
+++ master.py
@@ -5,6 +5,9 @@ from src.tracking.tracker import Tracker
 from src.telemetry.sd_receiver import SDReceiver
 from src.telemetry.sd_transmitter import SDTransmitter
 from src.dashboards.sensor_motor_dashboard import SensorMotorDashboard
+from src.data_logging.data_logging import DataLogger
+from src.data_logging.flight_model_training import FlightModelTrainer
+from src.data_logging.report_generation import ReportGenerator
 
 autopilot = Autopilot()
 return_to_home = ReturnToHome()
@@ -13,6 +16,9 @@ tracker = Tracker()
 sd_receiver = SDReceiver()
 sd_transmitter = SDTransmitter()
 sensor_motor_dashboard = SensorMotorDashboard()
+data_logger = DataLogger()
+flight_model_trainer = FlightModelTrainer()
+report_generator = ReportGenerator()
 
 def execute_flight():
     print("Initiating Rocket Flight Sequence...")
@@ -25,5 +31,19 @@ def execute_flight():
     return_to_home.setup()
     print("Rocket Flight Sequence Active.")
 
+def pre_flight_operations():
+    print("Initiating Pre-Flight Operations...")
+    # implement something here at some point
+
+def post_flight_operations():
+    print("Initiating Post-Flight Operations...")
+    data_logger.initialize()
+    flight_model_trainer.train_model()
+    report_generator.generate()
+    print("Post-Flight Operations Complete.")
+
 if __name__ == "__main__":
     execute_flight()
+    # Uncomment the line below to run post-flight operations after the flight
+    # post_flight_operations()
+    # pre_flight_operations()
